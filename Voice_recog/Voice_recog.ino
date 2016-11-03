f3257135bd499b6b30b99c4236e55909a008b12b
Deepak Srivatsav <deepak.srivatsav@gmail.com>
2016-11-04 01:39:57 +0530

    Removed * from string, added lcd support

--- Voice_recog/Voice_recog.ino
+++ Voice_recog/Voice_recog.ino
@@ -1,18 +1,22 @@
 #include <SoftwareSerial.h>
+#include "rgb_lcd.h"
+rgb_lcd lcd;
 
 SoftwareSerial Genotronex(10, 11);
 int led1 = 13;
-String BluetoothData;
+String BluetoothData, newdata;
 
 void setup() {
   
   Genotronex.begin(38400);
   pinMode(led1, OUTPUT);
+  lcd.begin(16, 2);
 }
 
 void loop() 
 {
-  
+  lcd.clear();
+  lcd.setRGB(255, 255, 255);
   while (Genotronex.available())
   {
     char c = Genotronex.read();
@@ -28,7 +32,15 @@ void loop()
       digitalWrite(led1, 0);      
     }
 delay(100);
+newdata="";
+for ( int i=1; BluetoothData[i]!='\0';i++)
+{
+   newdata+=BluetoothData[i];
+}
+
+lcd.setCursor(0,0);
+lcd.print(newdata);
+delay(2000);
 BluetoothData="";
 }
-  
 
