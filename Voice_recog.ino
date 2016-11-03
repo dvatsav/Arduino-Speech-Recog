d9ab1c5fb3febb1f58612b34e1b72ef93ffc1c36
Deepak Srivatsav <deepak16030@iiitd.ac.in>
2016-11-04 00:28:21 +0530

    Add files via upload

---
+++ Voice_recog.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial Genotronex(10, 11);
+int led1 = 13;
+String BluetoothData;
+
+void setup() {
+  
+  Genotronex.begin(38400);
+  pinMode(led1, OUTPUT);
+}
+
+void loop() 
+{
+  
+  while (Genotronex.available())
+  {
+    char c = Genotronex.read();
+    if (c == '#'){break;}
+    BluetoothData+=c;
+  }  
+    if (BluetoothData == "*TV on") 
+    {
+      digitalWrite(led1, 1);
+    }
+    if (BluetoothData == "*TV off") 
+    { 
+      digitalWrite(led1, 0);      
+    }
+delay(100);
+BluetoothData="";
+}
+  
+
