7e0b1fcab663073111105f3ee20c12d3e2f9c4dd
YashitM <yashitmaheshwary@gmail.com>
2016-11-04 03:01:44 +0530

    Added C Pseudo Code

--- Voice_recog/Voice_recog.ino
+++ Voice_recog/Voice_recog.ino
@@ -3,7 +3,7 @@
 rgb_lcd lcd;
 
 SoftwareSerial Genotronex(10, 11);
-int led1 = 13;
+int led1 = 13,flag;
 String BluetoothData, newdata;
 
 void setup() {
@@ -15,6 +15,7 @@ void setup() {
 
 void loop() 
 {
+  flag=0;
   lcd.clear();
   lcd.setRGB(255, 255, 255);
   while (Genotronex.available())
@@ -23,6 +24,8 @@ void loop()
     if (c == '#'){break;}
     BluetoothData+=c;
   }  
+
+  /*
     if (BluetoothData == "*TV on") 
     {
       digitalWrite(led1, 1);
@@ -30,13 +33,40 @@ void loop()
     if (BluetoothData == "*TV off") 
     { 
       digitalWrite(led1, 0);      
-    }
+    }*/
 delay(100);
 newdata="";
 for ( int i=1; BluetoothData[i]!='\0';i++)
 {
    newdata+=BluetoothData[i];
+   if(BluetoothData[i]=="+")
+   {
+    flag=1;
+   }
+   else if(BluetoothData[i]=="-")
+   {
+    flag=2;
+   }
+   else if(BluetoothData[i]=="x")
+   {
+    flag=3;
+   }   
 }
+if(flag==1)
+{
+  for (int i=0; newdata[i]!='\0';i++)
+  {
+    if(newdata[i]="+")
+    {
+      for(int j=0;j<=i-2;j++)
+      {
+        number1+=newdata[j];
+      }
+    }
+  }
+}
+
+
 
 lcd.setCursor(0,0);
 lcd.print(newdata);
