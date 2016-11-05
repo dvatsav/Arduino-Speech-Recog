8088fcd96a658da3e1af4892c2675b2ada4d47f2
Deepak Srivatsav <deepak16030@iiitd.ac.in>
2016-11-05 23:49:25 +0530

    Update AT Commands.ino

--- AT Commands.ino
+++ AT Commands.ino
@@ -1,24 +1,21 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(10, 11); // RX | TX
+SoftwareSerial BTSerial(10, 11); 
 
 void setup()
 {
-  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  pinMode(9, OUTPUT);  
   digitalWrite(9, HIGH);
-  Serial.begin(9600);
+  Serial.begin(9600); 
   Serial.println("Enter AT commands:");
-  BTSerial.begin(38400);  // HC-05 default speed in AT command more
+  BTSerial.begin(38400);  
 }
 
 void loop()
 {
-
-  // Keep reading from HC-05 and send to Arduino Serial Monitor
   if (BTSerial.available())
     Serial.write(BTSerial.read());
 
-  // Keep reading from Arduino Serial Monitor and send to HC-05
   if (Serial.available())
     BTSerial.write(Serial.read());
 }
