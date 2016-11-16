150015e5fd6b61d94ca8782064f2a2e3767c4910
Deepak Srivatsav <deepak16030@iiitd.ac.in>
2016-11-16 19:48:38 +0530

    Delete arduino to python.py

--- arduino to python.py
+++
@@ -1,8 +0,0 @@
-import serial
-import bluetooth
-arduino = serial.Serial('COM3', 9600, timeout=.1)
-while True:
-	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	newdata = data.decode()
-	if newdata:
-		print (newdata)
